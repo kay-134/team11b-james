aec70a09f47c19312c6ede8169602b3232e93227
Kendric Terry <kendric285@gmail.com>
2019-07-24 13:52:36 -0400

    hbi

--- js/planner.js
+++ js/planner.js
@@ -1,4 +1,3 @@
-
 let today = new Date();
 let currentMonth = today.getMonth();
 let currentYear = today.getFullYear();
@@ -71,6 +70,9 @@ function showCalendar(month, year) {
                 if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
                     cell.classList.add("bg-info");
                 } // color today's date
+                else if (date === 1 && year === today.getFullYear() && month === today.getMonth() ){
+                    cell.classList.add('july-01')
+                }
                 cell.appendChild(cellText);
                 row.appendChild(cell);
                 date++;
@@ -81,39 +83,5 @@ function showCalendar(month, year) {
 
         tbl.appendChild(row); // appending each row into calendar body.
     }
-    
 
 }
-
-$(document).ready(function(){
-    getWeather();
-
-
-function getWeather(){
-   //getting the city or area key
-    $.get(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=WgWCsW328fzBKdm4t3Aoci34y8ooI2ft&q=chelsea`, function(city){
-      let statekey = city[0].Key;
-
-      console.log(city[0].Key);
-      console.log(city);
-      //getting city or area weather information 
-        $.get(`https://dataservice.accuweather.com/forecasts/v1/daily/1day/${statekey}?apikey=WgWCsW328fzBKdm4t3Aoci34y8ooI2ft`, function(weather){
-              let minimumValue = weather.DailyForecasts[0].Temperature.Minimum.Value;
-              let maximumValue = weather.DailyForecasts[0].Temperature.Maximum.Value;
-              let summary = weather.Headline.Text
-              let dayPrecipitation = weather.DailyForecasts[0].Day.HasPrecipitation;
-              let nightPrecipitation = weather.DailyForecasts[0].Night.HasPrecipitation;
-              let precipitationTypeDay =  weather.DailyForecasts[0].Day.PrecipitationType
-              let precipitationTypeNight = weather.DailyForecasts[0].Night.PrecipitationType
-              let iconNum = weather.DailyForecasts[0].Day.Icon
-              let iconPhrase = weather.DailyForecasts[0].Day.IconPhrase
-              console.log(maximumValue)
-              console.log(minimumValue);
-
-            });
-      });
-  
-
-  };
-
-});
